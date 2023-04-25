c56dab2cf9217cf127939c9920348f90e06a27bf
Raj <39922631+raj-28@users.noreply.github.com>
2023-04-25 07:35:00 +0000

    zip decompressed

---
+++ folio/src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import profilePic1 from "../../img/gurjari.png";
+import profilePic2 from "../../img/profile2.jpg";
+import profilePic3 from "../../img/profile3.jpg";
+import profilePic4 from "../../img/profile4.jpg";
+
+const Testimonial = () => {
+  const clients = [
+    {
+      img: profilePic1,
+      review:
+        "Raj Patel is a joy to work with, he is an excellent recourse and a master at Python/Django programming... He handled our Web development with aplomb and diligence, displaying the ability to follow creative direction while adding his own creative input in a diplomatic and useful way. His work led to an increase in click-troughs. I recommend him to anyone who needs web Development and Website work.",
+    },
+    {
+      img: profilePic2,
+      review:
+        "Raj is my best friend, best mentor, best influencer and great human being. He is among those people who can handle any team, can teach anyone so nicely. We are working together since 4 years indirectly and I'm sure this bond will never break.",
+    },
+    {
+      img: profilePic3,
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+    },
+    {
+      img: profilePic4,
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+    },
+  ];
+
+  return (
+    <div className="t-wrapper" id="testimonial">
+      <div className="t-heading">
+        <span>Clients always get </span>
+        <span>Exceptional Work </span>
+        <span>from me...</span>
+        <div
+          className="blur t-blur1"
+          style={{ background: "var(--purple)" }}
+        ></div>
+        <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+      </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="testimonial">
+                <img src={client.img} alt="" />
+                <span>{client.review}</span>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonial;
---
+++ folio/src/components/Works/Works.jsx
@@ -0,0 +1,83 @@
+import React, { useContext } from "react";
+import "./Works.css";
+import Upwork from "../../img/Upwork.png";
+// import Fiverr from "../../img/fiverr.png";
+import teenager from "../../img/teenager.png";
+// import Amazon from "../../img/amazon.png";
+import Infosys from "../../img/infosys.png";
+// import Shopify from "../../img/Shopify.png";
+import uncledukan from "../../img/uncledukan.png";
+// import Facebook from "../../img/Facebook.png";
+import gurjari from "../../img/gurjari.png"
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import {Link} from 'react-scroll'
+const Works = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  return (
+    <div className="works" id="works">
+      {/* left side */}
+      <div className="w-left">
+        <div className="awesome">
+          {/* dark Mode */}
+          <span style={{ color: darkMode ? "white" : "" }}>
+            Works for All these Brands as 
+          </span>
+          <span>Software Engineer & Entreprenuer</span>
+          <spane>
+          <b>Infosys:</b> Frontend Web Developer & Corporate Trainer 
+            <br />
+          <b>Gurjari Services:</b> Software Engineer & Web Developer in house
+            <br />
+           <b>Teenager Startups:</b> Project Lead & Agile Enthusiast at Teenager Startups
+            <br />
+            <b>Statup UncleDukan:</b> Founder & E-commerce Strategist at Statup UncleDukan          </spane>
+          <Link to="contact" smooth={true} spy={true}>
+            <button className="button s-button">Hire Me</button>
+          </Link>
+          <div
+            className="blur s-blur1"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+
+        {/* right side */}
+      </div>
+      <div className="w-right">
+        <motion.div
+          initial={{ rotate: 45 }}
+          whileInView={{ rotate: 0 }}
+          viewport={{ margin: "-40px" }}
+          transition={{ duration: 3.5, type: "spring" }}
+          className="w-mainCircle"
+        >
+          <div className="w-secCircle">
+            <img src={uncledukan} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={teenager} alt="" />
+          </div>
+          <div className="w-secCircle">
+             <img src={Infosys} alt="" />
+
+          </div>{" "}
+          <div className="w-secCircle">
+            <img src={Upwork} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={gurjari} alt="" />
+          </div>
+        </motion.div>
+        {/* background Circles */}
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
